8f4239b9925db7512383287f2330720561ac0993
莯凛 <mokurin000@gmail.com>
2026-03-07 01:29:45 +0800

    chore: adb fetch screenshot

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import subprocess
+
 import easyocr
 import numpy as np
+
+
+def fetch_screenshot():
+    subprocess.run(
+        ["adb", "shell", "screencap", "-p", "/data/local/tmp/screenshot.png"],
+    )
+    subprocess.run(
+        ["adb", "pull", "/data/local/tmp/screenshot.png"],
+    )
 
 
 def init_reader() -> easyocr.Reader:
@@ -7,9 +18,14 @@ def init_reader() -> easyocr.Reader:
     return reader
 
 
+fetch_screenshot()
+
 reader = init_reader()
-result = reader.readtext("Telegram_gvC1JfcFZ3.png")
+result = reader.readtext("screenshot.png")
 
 for coords, text, confidence in result:
+    top_left, top_right, bottom_left, bottom_right = coords
+    # todo: 调用 cv2 绘制边框到识别处
+    print(top_left, top_right, bottom_left, bottom_right)
     confidence: np.float64
     print(text, f"{confidence * 100:.02f}%")
