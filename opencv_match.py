88b8d33bff27af806aa38342988ebde4979941df
莯凛 <mokurin000@gmail.com>
2026-03-23 18:13:46 +0800

    test: add more matching tests

--- opencv_match.py
+++ opencv_match.py
@@ -2,11 +2,19 @@ import os
 
 import cv2 as cv
 import matplotlib.pyplot as plt
+from loguru import logger
 
 
-def match_weather(weather: str, threshold=0.8, show=True):
+def match_weather(
+    weather: str,
+    threshold=0.8,
+    test_weather: str = None,
+    show=True,
+):
     template = cv.imread(f"weather/{weather}.png", cv.IMREAD_GRAYSCALE)
-    image = cv.imread(f"weather-test/{weather}.png", cv.IMREAD_GRAYSCALE)
+    image = cv.imread(
+        f"weather-test/{test_weather or weather}.png", cv.IMREAD_GRAYSCALE
+    )
 
     if template is None or image is None:
         print("图片读取失败")
@@ -38,6 +46,25 @@ def match_weather(weather: str, threshold=0.8, show=True):
 
 
 if __name__ == "__main__":
-    for weather in os.listdir("weather-test"):
-        weather = weather.removesuffix(".png")
-        match_weather(weather)
+    test_pics = [w.removesuffix(".png") for w in os.listdir("weather-test")]
+
+    # matching test
+    for weather in test_pics:
+        try:
+            assert match_weather(weather, show=False)
+        except AssertionError:
+            logger.error(f"{weather} matching failed!")
+        else:
+            logger.info(f"{weather} matching passed!")
+
+    # not matching test
+    for weather in test_pics:
+        others = set(test_pics)
+        others.remove(weather)
+        for other in others:
+            try:
+                assert not match_weather(weather, test_weather=other)
+            except AssertionError:
+                logger.error(f"{weather} in {other} not matching failed!")
+            else:
+                logger.info(f"{weather} in {other} not matching passed!")
