52c13f38bcbee8e39a7fd04f763db0b8166b0b40
莯凛 <mokurin000@gmail.com>
2026-03-23 16:33:42 +0800

    feat: crop from previous screenshots

--- tools/save_weather.py
+++ tools/save_weather.py
@@ -1,9 +1,14 @@
+from sys import argv
 from io import BytesIO
 
 from PIL import Image
 
 from fknc_adb_helper.utils import take_screenshot, WEATHER_RECT
 
-pic = take_screenshot()
-img = Image.open(BytesIO(pic))
+if len(argv) < 2:
+    pic = take_screenshot()
+    img = Image.open(BytesIO(pic))
+else:
+    img = Image.open(argv[1])
+
 img.crop(WEATHER_RECT).save("weather/_.png")
