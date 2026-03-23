3e69bb97301b992f4d5bb14f97149b8559b5a086
莯凛 <mokurin000@gmail.com>
2026-03-23 21:40:58 +0800

    todo: detect weather, auto initialization

--- src/fknc_adb_helper/matching.py
+++ src/fknc_adb_helper/matching.py
@@ -1,5 +1,42 @@
+from os import listdir
+
 import cv2 as cv
+from loguru import logger
 from cv2.typing import MatLike
+
+NAME_MAP = {
+    "darkfog": "暗雾",
+    "eclipse": "日蚀",
+    "firefly": "萤光",
+    "meteor-shower": "流星雨",
+    "rainball": "彩虹",
+    "sandstorm": "沙尘暴",
+    "solarflare": "太阳耀斑",
+    "thunderstorm": "雷雨",
+}
+
+_WEATHERS = None
+
+
+def detect_weather(threshold: float = 0.8):
+    pass
+
+
+def init_weathers() -> dict[str, MatLike]:
+    weather_map = {}
+    weather_icons = listdir("weather/")
+    for weather_img in weather_icons:
+        weather_name = weather_img.removesuffix(".png")
+
+        if weather_name not in NAME_MAP:
+            logger.warning(f"unknown weather: {weather_name}")
+            continue
+        weather_name = NAME_MAP[weather_name]
+
+        template = cv.imread(f"weather/{weather_img}", cv.IMREAD_GRAYSCALE)
+        weather_map[weather_name] = template
+
+    return weather_map
 
 
 def match_object(
