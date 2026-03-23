256b75135f2c71d89db49202838c3d367071d89c
莯凛 <mokurin000@gmail.com>
2026-03-23 17:13:59 +0800

    chore: add more weather icons

--- tools/save_weather.py
+++ tools/save_weather.py
@@ -6,9 +6,14 @@ from PIL import Image
 from fknc_adb_helper.utils import take_screenshot, WEATHER_RECT
 
 if len(argv) < 2:
+    exit(1)
+
+name = argv[1]
+
+if len(argv) < 3:
     pic = take_screenshot()
     img = Image.open(BytesIO(pic))
 else:
-    img = Image.open(argv[1])
+    img = Image.open(argv[2])
 
-img.crop(WEATHER_RECT).save("weather/_.png")
+img.crop(WEATHER_RECT).save(f"weather/{name}.png")
