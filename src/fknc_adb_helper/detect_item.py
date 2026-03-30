964599adbfaaf07ddff2d3acb83bfe627325ef79
莯凛 <mokurin000@gmail.com>
2026-03-30 15:45:29 +0800

    fix: bad missing check

--- src/fknc_adb_helper/detect_item.py
+++ src/fknc_adb_helper/detect_item.py
@@ -4,6 +4,7 @@ from PIL import Image
 def item_exists(img: Image.Image):
     for pixel in img.getdata():
         if pixel in [
+            (207, 212, 227, 255),
             (232, 237, 255, 255),
             (232, 238, 255, 255),
             (233, 237, 255, 255),
--- test_existing.py
+++ test_existing.py
@@ -6,10 +6,12 @@ from fknc_adb_helper.detect_item import item_exists
 def _test():
     exists = Image.open("test/existing.png")
     exists2 = Image.open("test/existing2.png")
+    exists3 = Image.open("test/existing3.png")
     not_exists = Image.open("test/non-exists.png")
 
     assert item_exists(exists)
     assert item_exists(exists2)
+    assert item_exists(exists3)
     assert not item_exists(not_exists)
 
     print("test passed!")
